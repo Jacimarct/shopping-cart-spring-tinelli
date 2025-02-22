05c8ffe6f89e125f64c4892893d3a0e926e0bd83
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2025-02-21 21:10:22 -0300

    da hora 20250221 21:10

--- src/main/java/com/ecom/TesteConexaoRailway.java
+++ src/main/java/com/ecom/TesteConexaoRailway.java
@@ -3,6 +3,8 @@ package com.ecom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.springframework.boot.SpringApplication;
+
 public class TesteConexaoRailway {
     public static void main(String[] args) {
         // Substitua esses valores pelas suas variáveis de ambiente do Railway
@@ -12,6 +14,7 @@ public class TesteConexaoRailway {
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Conexão bem-sucedida!");
+            SpringApplication.run(ShoppingCartApplication.class, args);            
         } catch (Exception e) {
             System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
             e.printStackTrace();
