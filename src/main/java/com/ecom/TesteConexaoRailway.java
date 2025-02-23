c1d055c96e030274eecb759f6cddcb946e978555
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2025-02-23 17:29:09 -0300

    20250223 17:29

--- src/main/java/com/ecom/TesteConexaoRailway.java
+++ src/main/java/com/ecom/TesteConexaoRailway.java
@@ -8,10 +8,12 @@ import org.springframework.boot.SpringApplication;
 public class TesteConexaoRailway {
     public static void main(String[] args) {
         // Substitua esses valores pelas suas variáveis de ambiente do Railway
-        String url = "jdbc:mysql://yamabiko.proxy.rlwy.net:41270/railway?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-        String username = "root";
-        String password = "NzgoPyIWnkYKKYXMYeVRUGjyimShOoHA";
-
+		
+		  String url =
+		  "jdbc:mysql://mainline.proxy.rlwy.net:55422/railway?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+		  String username = "root"; 
+		  String password = "EFdjSUWzbwluqESGAQYPPIRqnevCjpOW";
+		 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Conexão bem-sucedida!");
             SpringApplication.run(ShoppingCartApplication.class, args);            
